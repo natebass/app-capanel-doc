da8951052fdf8e73351f33336f55c5f5e372de83
Nate Bass <nw3455@gmail.com>
2026-03-09 23:12:50 -0700

    Fix the dark mode theme.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,48 +1,46 @@
-import { TanStackDevtools } from "@tanstack/react-devtools";
-import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { DefaultPending } from "@/components/layout/status/DefaultPending";
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import type { QueryClient } from '@tanstack/react-query'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
-import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
-import { Provider } from "../integrations/tanstack-query/root-provider";
-import AiDevtools from "../lib/ai-devtools";
-import StoreDevtools from "../lib/demo-store-devtools";
+import { DefaultPending } from '@/components/layout/status/DefaultPending'
+import { ThemeProvider } from '@/components/theme-provider'
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import { Provider } from '../integrations/tanstack-query/root-provider'
+import StoreDevtools from '../lib/demo-store-devtools'
 
 interface MyRouterContext {
-	queryClient: QueryClient;
+	queryClient: QueryClient
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
 	pendingComponent: () => (
-		<DefaultPending
-			fullPage={false}
-			variant="card"
-			message="Loading application..."
-		/>
+		<DefaultPending fullPage={false} variant='card' message='Loading application...' />
 	),
 	component: RootComponent,
-});
+})
 
 function RootComponent() {
-	const { queryClient } = Route.useRouteContext();
+	const { queryClient } = Route.useRouteContext()
 	return (
-		<Provider queryClient={queryClient}>
-			<Outlet />
-			<TanStackDevtools
-				config={{
-					position: "bottom-right",
-				}}
-				plugins={[
-					{
-						name: "Tanstack Router",
-						render: <TanStackRouterDevtoolsPanel />,
-					},
-					TanStackQueryDevtools,
-					StoreDevtools,
-					AiDevtools,
-				]}
-			/>
-		</Provider>
-	);
+		<ThemeProvider>
+			<Provider queryClient={queryClient}>
+				<Outlet />
+				<TanStackDevtools
+					config={{
+						position: 'bottom-right',
+					}}
+					plugins={[
+						{
+							name: 'Tanstack Router',
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+						TanStackQueryDevtools,
+						StoreDevtools,
+					]}
+				/>
+			</Provider>
+		</ThemeProvider>
+	)
 }
