625cb0a169fd5090b60fe7d19fae783f846fd4f7
Nate Bass <nw3455@gmail.com>
2026-09-12 16:00:01 -0700

    Add the Storybook Sync package to work with the IntelliJ Storybook Connect plugin.

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,7 +5,12 @@ import type { StorybookConfig } from '@storybook/react-vite'
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
 
-	addons: ['@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-vitest'],
+	addons: [
+		'addon-storybook-sync',
+		'@storybook/addon-docs',
+		'@storybook/addon-a11y',
+		'@storybook/addon-vitest',
+	],
 
 	framework: {
 		name: '@storybook/react-vite',
